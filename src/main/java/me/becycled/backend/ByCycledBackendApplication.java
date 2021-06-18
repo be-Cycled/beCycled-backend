63b7d4d07b5637efff84d6c6127c96b0aeef4263
Ivan Muratov <binakot@gmail.com>
2021-06-18 17:44:45 +0300

    Init commit

---
+++ src/main/java/me/becycled/backend/ByCycledBackendApplication.java
@@ -0,0 +1,13 @@
+package me.becycled.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ByCycledBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ByCycledBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/becycled/backend/ByCycledBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package me.becycled.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ByCycledBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
