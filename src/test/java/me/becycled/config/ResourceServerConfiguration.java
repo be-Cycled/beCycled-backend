eba25f421d8dcc082ff09f6d11de57ac1d45fbf1
l1yi4 <gehrman.ilya@gmail.com>
2021-06-19 23:38:22 +0300

    Первые тесты rest api

---
+++ src/test/java/me/becycled/config/ResourceServerConfiguration.java
@@ -0,0 +1,36 @@
+package me.becycled.config;
+
+import me.becycled.backend.config.security.OAuthConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+
+/**
+ * @author I1yi4
+ */
+@Configuration
+@EnableResourceServer
+@Profile("test")
+public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
+
+    @Override
+    public void configure(final ResourceServerSecurityConfigurer resources) {
+        resources.resourceId(OAuthConfiguration.RESOURCE_ID);
+    }
+
+    @Override
+    public void configure(final HttpSecurity http) throws Exception {
+        http
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
+            .antMatcher("/**")
+            .authorizeRequests()
+            .antMatchers( "/**").permitAll()
+            .anyRequest().authenticated();
+    }
+}
---
+++ src/test/java/me/becycled/controller/RouteControllerTest.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright © 2018 ООО "Первая Мониторинговая Компания".
+ * All rights reserved.
+ */
+
+package me.becycled.controller;
+
+import me.becycled.BaseIntegrationTest;
+import me.becycled.ByCycledBackendApplicationTest;
+import me.becycled.backend.model.entity.route.Route;
+import me.becycled.backend.model.entity.user.User;
+import me.becycled.utils.TestUtils;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.*;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.net.URI;
+
+import static org.assertj.core.api.BDDAssertions.then;
+
+/**
+ * @author I1yi4
+ */
+@ActiveProfiles("test")
+@RunWith(SpringRunner.class)
+@SpringBootTest(
+    classes = ByCycledBackendApplicationTest.class,
+    webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class RouteControllerTest extends BaseIntegrationTest {
+
+    @Test
+    public void getById() {
+        User user = TestUtils.getTestUser();
+        user = daoFactory.getUserDao().create(user);
+
+        Route route = TestUtils.getTestRoute();
+        route = daoFactory.getRouteDao().create(route);
+
+        final ResponseEntity<Route> response = restTemplate.exchange(
+            "http://localhost:" + port + "/routes/" + route.getId(),
+            HttpMethod.GET, HttpEntity.EMPTY, Route.class);
+
+        then(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        then(response.getBody()).isEqualTo(route);
+    }
+
+    @Test
+    public void create() {
+        User user = TestUtils.getTestUser();
+        user = daoFactory.getUserDao().create(user);
+
+        Route route = TestUtils.getTestRoute();
+
+        final RequestEntity<Route> request = RequestEntity
+            .post(URI.create("http://localhost:" + port + "/routes"))
+            .body(route);
+
+        final ResponseEntity<String> response = restTemplate.exchange(
+            request, String.class);
+
+        then(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
+}
